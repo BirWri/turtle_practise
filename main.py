61db012aeb9a3065c78689eaf3fbd025f5a6f652
BirWri <55280458+BirWri@users.noreply.github.com>
2024-09-13 17:03:38 +0200

    Turtle draw a dashed line

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from turtle import Turtle, Screen
 
+# Turtle features
 tim = Turtle()
+tim.hideturtle()
 tim.shape("turtle")
 tim.color("red")
 
-#Turtle draws a square
-for x in range(4):
-    tim.forward(100)
-    tim.right(90)
+# Turtle starting position
+tim.penup()
+tim.setx(-300)
+tim.sety(0)
+tim.showturtle()
 
+# Turtle drawing a dashed line
+while tim.xcor() < 300:
+    print(tim.xcor())
+    tim.pendown()
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
 
 screen = Screen()
 screen.exitonclick()
